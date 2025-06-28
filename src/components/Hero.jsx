d45a1b434c2a0da448169d00da728b4873dc7ae5
cycy <96785236+lovecycy@users.noreply.github.com>
2025-06-29 00:59:43 +0800

    优化 Hero 组件，添加视频播放控制和响应式设计支持

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,15 @@
 import { useGSAP } from "@gsap/react"
 import gsap from "gsap"
 import { SplitText } from "gsap/all"
+import { useRef } from "react"
+import { useMediaQuery } from "react-responsive"
 
 export default function Hero() {
+    const videoRef = useRef()
+    const isMobile = useMediaQuery({ maxWidth: 767 })
+
     useGSAP(() => {
-        const heroSplit = new SplitText('.title', { type: 'chars' })
+        const heroSplit = new SplitText('.title', { type: 'chars, words' })
         // 添加颜色渐变
         heroSplit.chars.forEach(char => char.classList.add('text-gradient'))
         // 为每一个单词添加渐入动画
@@ -35,28 +40,57 @@ export default function Hero() {
             })
             .to('.right-leaf', { y: 200 }, 0)
             .to('.left-leaf', { y: -200 }, 0)
+
+        const startValue = isMobile ? 'top 50%' : 'center 60%'
+        const endValue = isMobile ? '120% top' : 'bottom top'
+        let tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: "video",
+                start: startValue,
+                end: endValue,
+                scrub: true,
+                pin: true,
+            },
+        });
+        videoRef.current.onloadedmetadata = () => {
+            tl.to(videoRef.current, {
+                currentTime: videoRef.current.duration,
+            });
+        };
     }, [])
     return (
-        <section id="hero" className="noisy">
-            <h1 className="title">MOJITO</h1>
-            <img src="/images/hero-left-leaf.png" alt="left-leaf" className="left-leaf" />
-            <img src="/images/hero-right-leaf.png" alt="right-leaf" className="right-leaf" />
-            <div className="body">
-                <div className="content">
-                    <div className="space-y-5 hidden md:block">
-                        <p>Cool. Crisp. Classic</p>
-                        <p className="subtitle">
-                            Sip the  Spirit <br />of Summer
-                        </p>
-                    </div>
-                    <div className="view-cocktails">
-                        <p className="subtitle">
-                            Every cocktail on our menu is a blend of premium ingredients, creative flair, and timeless recipes - designed to delight your senses.
-                        </p>
-                        <a href="#cocktails">View Cocktails</a>
+        <>
+            <section id="hero" className="noisy">
+                <h1 className="title">MOJITO</h1>
+                <img src="/images/hero-left-leaf.png" alt="left-leaf" className="left-leaf" />
+                <img src="/images/hero-right-leaf.png" alt="right-leaf" className="right-leaf" />
+                <div className="body">
+                    <div className="content">
+                        <div className="space-y-5 hidden md:block">
+                            <p>Cool. Crisp. Classic</p>
+                            <p className="subtitle">
+                                Sip the  Spirit <br />of Summer
+                            </p>
+                        </div>
+                        <div className="view-cocktails">
+                            <p className="subtitle">
+                                Every cocktail on our menu is a blend of premium ingredients, creative flair, and timeless recipes - designed to delight your senses.
+                            </p>
+                            <a href="#cocktails">View Cocktails</a>
+                        </div>
                     </div>
                 </div>
+            </section>
+            <div className="video absolute inset-0">
+                <video
+                    ref={videoRef}
+                    src="/videos/output.mp4"
+                    muted
+                    playsInline
+                    preload="auto"
+                >
+                </video>
             </div>
-        </section>
+        </>
     )
 }
