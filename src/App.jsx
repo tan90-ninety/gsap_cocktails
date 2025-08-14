950e4666343a76778e3daecf8b1ab402b593c98b
cycy <96785236+lovecycy@users.noreply.github.com>
2025-08-14 17:36:24 +0800

    添加 About 组件并在 App 组件中引入

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { ScrollTrigger, SplitText } from 'gsap/all'
 import Navbar from './components/Navbar.jsx'
 import Hero from './components/Hero.jsx'
 import Cocktails from './components/Cocktails.jsx'
+import About from './components/About.jsx'
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Navbar></Navbar>
       <Hero></Hero>
       <Cocktails></Cocktails>
+      <About></About>
     </main>
   )
 }
